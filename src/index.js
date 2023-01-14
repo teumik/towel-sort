6a3e20f1e293bb8d539c2b6b95466d6a03fb60c7
Artem Mokhov <teumik@gmail.com>
2023-01-15 03:04:09 +0400

    refactor: remove comments

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-
 // You should implement your task here.
 module.exports = function towelSort(matrix) {
   if (!matrix) {
@@ -10,17 +9,11 @@ module.exports = function towelSort(matrix) {
   matrix.forEach((el, i) => {
     if (i % 2 !== 0) {
       arr = arr.concat(matrix[i].sort((a, b) => b - a));
-    } 
+    }
     else {
       arr = arr.concat(matrix[i]);
     }
   });
 
   return arr;
-
-  // return matrix.reduce((acc, el, i) => {
-  //   el.sort((a, b) => (i % 2 !== 0) ? b - a : a - b).map(item => acc.push(item));
-  //   return acc;
-  // }, [])
-
 }
